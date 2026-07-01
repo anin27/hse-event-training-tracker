bb7bee3cb242e4ae0c77b32cea92bae750947880
anin27 <anin.thomas19@gmail.com>
2026-07-01 15:12:37 +0100

    Style: add navbar and navbar left styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
 import Login from './Login';
+import Dashboard from './Dashboard';
 
 function App() {
   const [isAuth, setAuth] = useState(!!localStorage.getItem('token'));
 
-  if (!isAuth) {
-    return <Login setAuth={setAuth} />;
-  }
-
   return (
-    <div className="app">
-      <h1>Welcome!</h1>
-      <button onClick={() => {
-        localStorage.removeItem('token');
-        setAuth(false);
-      }}>
-        Logout
-      </button>
+    <div className="App">
+      {!isAuth ? <Login setAuth={setAuth} /> : <Dashboard />}
     </div>
   );
 }
