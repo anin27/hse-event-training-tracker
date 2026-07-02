07fd1a6b7dd56ea9ee97cde7412267c4cc54b1f6
anin27 <anin.thomas19@gmail.com>
2026-07-02 15:55:09 +0100

    Feature: add currentPage state for navigation

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 import Login from './Login';
 import Dashboard from './Dashboard';
+import Events from './Events';
 
 function App() {
   const [isAuth, setAuth] = useState(!!localStorage.getItem('token'));
+  const [currentPage, setCurrentPage] = useState('dashboard');
 
   return (
     <div className="App">
-      {!isAuth ? <Login setAuth={setAuth} /> : <Dashboard />}
+      {!isAuth ? (
+        <Login setAuth={setAuth} />
+      ) : (
+        <div>
+          {/* Navbar coming next */}
+        </div>
+      )}
     </div>
   );
-}
-
-export default App;
